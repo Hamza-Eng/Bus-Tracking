1ce2f7e34bd3e1240692d69d8599b32396956817
Hamza Madani <hamzamadani217@gmail.com>
2024-02-11 22:09:42 +0100

    first push

---
+++ src/main/java/akaza/com/main/BusTracking10022024Application.java
@@ -0,0 +1,13 @@
+package akaza.com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTracking10022024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTracking10022024Application.class, args);
+	}
+
+}
---
+++ src/test/java/akaza/com/main/BusTracking10022024ApplicationTests.java
@@ -0,0 +1,13 @@
+package akaza.com.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTracking10022024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
